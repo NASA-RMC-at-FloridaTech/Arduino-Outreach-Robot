29c356441c1c6f2f3079cb3e79df076255f9c83d
Evangeline Archer <eroxyivy@yahoo.com>
2018-12-04 00:42:21 -0500

    update to 0.0.3
    Replaced Steppers with DC Motors
    Finished all functions
    Created 2 more example scripts

--- RMC.cpp
+++ RMC.cpp
@@ -7,56 +7,178 @@
 
 #include "Arduino.h"
 #include <Servo.h>
-#include <Stepper.h>
 #include "RMC.h"
 
 Robot::Robot()
 {
-
+	Serial.begin(9600);
+	Serial.print("Hi there! I'm a Robot!");
+	pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void Robot::Claw(int pin)
 {
 	pinMode(pin, OUTPUT);
-	_clawPin = pin;
-	_servo.attach(pin);
+	pin_claw = pin;
+	clawServo.attach(pin);
 }
 
-void Robot::RightWheel(int p1, int p2, int p3, int p4)
+void Robot::RightWheel(int p1, int p2)
 {
 	pinMode(p1, OUTPUT);
 	pinMode(p2, OUTPUT);
-	pinMode(p3, OUTPUT);
-	pinMode(p4, OUTPUT);
-	_rMotorPins[0] = p1;
-	_rMotorPins[1] = p2;
-	_rMotorPins[2] = p3;
-	_rMotorPins[3] = p4;
-	Stepper _rMotor(stepSpeed, p1, p2, p3, p4);
+	pin_rMotor[0] = p1;
+	pin_rMotor[1] = p2;
 }
 
-void Robot::LeftWheel(int p1, int p2, int p3, int p4)
+void Robot::LeftWheel(int p1, int p2)
 {
 	pinMode(p1, OUTPUT);
 	pinMode(p2, OUTPUT);
-	pinMode(p3, OUTPUT);
-	pinMode(p4, OUTPUT);
-	_lMotorPins[0] = p1;
-	_lMotorPins[1] = p2;
-	_lMotorPins[2] = p3;
-	_lMotorPins[3] = p4;
-	Stepper _lMotor(stepSpeed, p1, p2, p3, p4);
+	pin_lMotor[0] = p1;
+	pin_lMotor[1] = p2;
 }
 
 void Robot::Bumper(int pin)
 {
-	pinMode(pin, INPUT);
-	_bumperPin = pin;
+	pinMode(pin, INPUT_PULLUP);
+	pin_bumperRight = pin;
+	pin_bumperLeft = pin;
+}
+
+void Robot::RightBumper(int pin)
+{
+	pinMode(pin, INPUT_PULLUP);
+	pin_bumperRight = pin;
+}
+
+void Robot::LeftBumper(int pin)
+{
+	pinMode(pin, INPUT_PULLUP);
+	pin_bumperLeft = pin;
+}
+
+void Robot::Buzzer(int pin)
+{
+	pinMode(pin, OUTPUT);
+	pin_buzzer = pin;
+}
+
+void Robot::GoStraight(int ms)
+{
+	digitalWrite(pin_lMotor[1], LOW);
+	digitalWrite(pin_rMotor[1], LOW);
+	analogWrite(pin_lMotor[0], moveSpeed);
+	analogWrite(pin_rMotor[0], moveSpeed);
+	delay(ms);
+}
+
+void Robot::GoBackwards(int ms)
+{
+	digitalWrite(pin_lMotor[0], LOW);
+	digitalWrite(pin_rMotor[0], LOW);
+	analogWrite(pin_lMotor[1], moveSpeed);
+	analogWrite(pin_rMotor[1], moveSpeed);
+	delay(ms);
+}
+
+void Robot::Step()
+{
+	digitalWrite(pin_lMotor[1], LOW);
+	digitalWrite(pin_rMotor[1], LOW);
+	analogWrite(pin_lMotor[0], moveSpeed);
+	analogWrite(pin_rMotor[0], moveSpeed);
+	delay(stepDuration);
+}
+
+void Robot::Steps(int steps)
+{
+	for (int i=0; i <= steps; i++)
+	{
+		digitalWrite(pin_lMotor[1], LOW);
+		digitalWrite(pin_rMotor[1], LOW);
+		analogWrite(pin_lMotor[0], moveSpeed);
+		analogWrite(pin_rMotor[0], moveSpeed);
+		delay(stepDuration);
+	}
+}
+
+void Robot::TurnLeft(int ms)
+{
+	digitalWrite(pin_lMotor[1], LOW);
+	digitalWrite(pin_rMotor[0], LOW);
+	analogWrite(pin_lMotor[0], moveSpeed);
+	analogWrite(pin_rMotor[1], moveSpeed);
+	delay(ms);
+}
+
+void Robot::TurnLeft()
+{
+	digitalWrite(pin_lMotor[1], LOW);
+	digitalWrite(pin_rMotor[0], LOW);
+	analogWrite(pin_lMotor[0], moveSpeed);
+	analogWrite(pin_rMotor[1], moveSpeed);
+	delay(turnDuration);
+}
+
+void Robot::TurnRight(int ms)
+{
+	digitalWrite(pin_lMotor[0], LOW);
+	digitalWrite(pin_rMotor[1], LOW);
+	analogWrite(pin_lMotor[1], moveSpeed);
+	analogWrite(pin_rMotor[0], moveSpeed);
+	delay(ms);
+}
+
+void Robot::TurnRight()
+{
+	digitalWrite(pin_lMotor[0], LOW);
+	digitalWrite(pin_rMotor[1], LOW);
+	analogWrite(pin_lMotor[1], moveSpeed);
+	analogWrite(pin_rMotor[0], moveSpeed);
+	delay(turnDuration);
+}
+
+void Robot::Blink()
+{
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(100);
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(100);
+}
+
+void Robot::Honk(int ms)
+{
+	digitalWrite(pin_buzzer, HIGH);
+	delay(ms);
+	digitalWrite(pin_buzzer, LOW);
+	delay(100);
+}
+
+void Robot::HonkNote(int freq)
+{
+	analogWrite(pin_buzzer, freq);
+	delay(100);
+	digitalWrite(pin_buzzer, LOW);
+	delay(100);
+}
+
+void Robot::HonkNote(int freq, int ms)
+{
+	analogWrite(pin_buzzer, freq);
+	delay(ms);
+	digitalWrite(pin_buzzer, LOW);
+	delay(100);
+}
+
+void Robot::PlayMelody()
+{
+
 }
 
 bool Robot::BumpedIntoSomething()
 {
-	if(digitalRead(_bumperPin) == LOW)
+	if(digitalRead(pin_bumperRight) == LOW || digitalRead(pin_bumperLeft) == LOW)
 	{
 		return false;
 	} else {
@@ -64,22 +186,37 @@ bool Robot::BumpedIntoSomething()
 	}
 }
 
-void Robot::TurnRight()
+bool Robot::BumpedIntoRight()
 {
-	
+	if(digitalRead(pin_bumperRight) == LOW)
+	{
+		return false;
+	} else {
+		return true;
+	}
 }
 
-void Robot::TurnLeft()
+bool Robot::BumpedIntoLeft()
 {
-	
+	if(digitalRead(pin_bumperLeft) == LOW)
+	{
+		return false;
+	} else {
+		return true;
+	}
 }
 
-void Robot::Step(int steps)
+void Robot::SetSpeed(int spd)
 {
-	
+	moveSpeed = spd;
 }
 
-void Robot::Step()
+void Robot::SetStepDuration(int ms)
 {
-	
+	stepDuration = ms;
+}
+
+void Robot::SetTurnDuration(int ms)
+{
+	turnDuration = ms;
 }
--- RMC.h
+++ RMC.h
@@ -10,36 +10,48 @@
 
 #include "Arduino.h"
 #include <Servo.h>
-#include <Stepper.h>
 
 class Robot
 {
   public:
     Robot();
-	void Claw(int pin);
-	void RightWheel(int p1, int p2, int p3, int p4);
-	void LeftWheel(int p1, int p2, int p3, int p4);
-	void Bumper(int pin);
-    void Turn(int ang);
-	void TurnLeft();
-	void TurnRight();
-	void Mine();
-	void Unload();
-	void Step();
-	void Step(int _steps);
-	void Blink();
-	void Honk();
-	int Capacity;
-	int totalMined;
-	bool BumpedIntoSomething();
+		void Claw(int pin);
+		void RightWheel(int p1, int p2);
+		void LeftWheel(int p1, int p2);
+		void Bumper(int pin);
+		void LeftBumper(int pin);
+		void RightBumper(int pin);
+		void Buzzer(int pin);
+		void GoStraight (int ms);
+		void GoBackwards (int ms);
+		void Step();
+		void Steps(int steps);
+		void TurnLeft(int ms);
+		void TurnRight(int ms);
+		void TurnLeft();
+		void TurnRight();
+		void Blink();
+		void Honk(int ms);
+		void HonkNote(int freq);
+		void HonkNote(int freq, int ms);
+		void PlayMelody();
+		bool BumpedIntoSomething();
+		bool BumpedIntoRight();
+		bool BumpedIntoLeft();
+		void SetSpeed(int spd);
+		void SetStepDuration(int ms);
+		void SetTurnDuration(int ms);
   private:
-    int _load;
-	int _clawPin;
-	const int stepSpeed = 200;
-	Servo _servo;
-	int _lMotorPins[4];
-	int _rMotorPins[4];
-	int _bumperPin;
+		int moveSpeed = 1023;
+		int stepDuration = 100;
+		int turnDuration = 250;
+		int pin_claw;
+		int pin_lMotor[2];
+		int pin_rMotor[2];
+		int pin_bumperRight;
+		int pin_bumperLeft;
+		int pin_buzzer;
+		Servo clawServo;
 };
 
 #endif
